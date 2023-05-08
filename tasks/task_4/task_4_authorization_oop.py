e0c490dc0e29f0d303e0018f3724d62a9e1cb97d
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-05-08 12:21:33 +0300

    Change realization "for" cycle. Add "if/else" for problem users in the end of Test_1

--- tasks/task_4/task_4_authorization_oop.py
+++ tasks/task_4/task_4_authorization_oop.py
@@ -1,16 +1,14 @@
 from login_page import LoginPage
 
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import Keys
 
 
 class Test1:
     driver = webdriver.Chrome()
+    problem_users = []
 
     # Check title 'Products'
     def check_title(self):
@@ -44,42 +42,24 @@ class Test1:
 
         for login in logins:
             # Authorization
+            print(f'****************Test login "{login}" - STARTED!****************')
             login_page.authorization(login, password_all)
 
-            print(f'****************Test login "{login}" - STARTED!****************')
-
-            if login == 'locked_out_user':
-                try:
-                    self.check_title()
-                    self.logOut()
-                except:
-                    # Get error msg and close it
-                    error_msg = WebDriverWait(self.driver, 2).until(
-                        EC.visibility_of_element_located((By.CSS_SELECTOR, "h3[data-test='error']")))
-                    print('Error message appeared')
-
-                    button_close_error = self.driver.find_element(By.CSS_SELECTOR, '.error-button')
-                    button_close_error.click()
-                    # Assert error_msg is disappeared
-                    assert WebDriverWait(self.driver, 2).until(EC.invisibility_of_element(error_msg)) is True
-                    print('Error message disappeared')
-                    # Clear LogIn inputs
-                    username = self.driver.find_element(By.ID, 'user-name')
-                    username.send_keys(Keys.CONTROL + "A")
-                    username.send_keys(Keys.BACKSPACE)
-
-                    password = self.driver.find_element(By.ID, 'password')
-                    password.send_keys(Keys.CONTROL + "A")
-                    password.send_keys(Keys.BACKSPACE)
-                    # Assert inputs is cleared
-                    assert len(username.get_attribute('value')) == 0 and len(password.get_attribute('value')) == 0
-                    print('Inputs cleared!')
-            else:
+            try:
                 self.check_title()
                 self.logOut()
+            except:
+                print(f'!!!!!!!!- User with login "{login}" isn\'t authorized -!!!!!!!!')
+                self.problem_users.append(login)
+                self.driver.refresh()
+
             print(f'****************Test login "{login}" - FINISHED!****************\n')
 
-        print('Test_1 Success!')
+        if self.problem_users:
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Test_1 partially successful !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            print(f'There are problems with user/users: {self.problem_users}')
+        else:
+            print('Test_1 Success!')
         print('++++++++++++++++ FINISH Test_1 ++++++++++++++++\n')
 
 
